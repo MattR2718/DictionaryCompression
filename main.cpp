6c2d889e749019147212dcfe3f591e316b013421
Matthew Ranson <matthew.ranson2718@gmail.com>
2021-12-24 18:25:30 +0000

    fixed compression calculation

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <algorithm>
 #include <map>
+#include <regex>
 
 //Returns true if not a letter or space
 bool notAlpha(char c) { return ((isalpha(c)) || (c == ' ')) ? false : true; }
@@ -57,8 +58,10 @@ int calcDictSize(std::map<std::string, uint16_t>& dict){
     return letterSum+extra;
 }
 
-std::string compressAll(const std::string& text, std::map<std::string, uint16_t>& dict){
-    std::string out = "";
+std::tuple<std::string, int, int> compressAll(const std::string& text, std::map<std::string, uint16_t>& dict){
+    std::string outText = "";
+    int nums = 0;
+    int chars = 0;
     for (int i = 0; i < text.length(); i++){
         if (isalpha(text[i])) { 
             int j = 0;
@@ -68,13 +71,20 @@ std::string compressAll(const std::string& text, std::map<std::string, uint16_t>
                 j++;
             }
             i += (j - 1);
-            out += std::to_string(dict[word]);
+            outText += std::to_string(dict[word]);
+            nums++;
         }
         else {
-            out += text[i];
+            outText += text[i];
+            chars++;
         }
     }
-    return out;
+    return std::make_tuple(outText, nums, chars);
+}
+
+void writeToFile(const std::string& text, const std::string name){
+    std::ofstream file(name);
+    file << text;
 }
 
 int main() {
@@ -83,20 +93,25 @@ int main() {
     std::string linetxt;
     std::string text = "";
     while(std::getline(file, linetxt)){
-        text += linetxt;
+        text += linetxt + " ";
     }
+    text.erase(text.length() - 1);
 
     //text = "Entering that gable-ended Spouter-Inn, you found yourself in a wide, low in in artist’s";
 
     std::string original = text;
 
+    //text += " " + text;
+
     //Step 1 - remove punctuation
-    //std::string parsedText = removePunct(text);
     //Replace all word splitting punctuation with spaces to split into 2 words
     replaceAll(text, "-'\"’");
-    //Remove all characters from text that are not a letter leaving just words
+    //Remove all characters from text that are not a letter or space
     text.erase(std::remove_if(text.begin(), text.end(), notAlpha), text.end());
-    //std::cout<<text<<'\n';
+    //Replace any groups of 2 or more spaces with a single space
+    text = std::regex_replace(text, std::regex("\\s{2,}"), " ");
+    
+    writeToFile(text, "../out.txt");
 
     //Step 2 - create dictionary
     //Make dictionary of all unique words
@@ -104,20 +119,17 @@ int main() {
     //printDict(dict);
 
     //Step 3 - calculate size of original text and dictionary
+    std::cout<<"Original Text Size: "<<text.length()<<" bytes\n";
     int dictionarySize = calcDictSize(dict);
     std::cout<<"Dictionary Size: "<<dictionarySize<<" bytes\n";
-    std::cout<<"Text Size : "<<text.length()<<" bytes\n";
 
     //Step 4 - fully compress and calculate size
-    std::string compressedText = compressAll(original, dict);
-    //ERROR HERE
-    //DONT TREAT EVERY NUMBER AS CHARACTERS
-    //EACH NUMBER IS 2 BYTES SO 547 IS 2 BYTES NOT 3 AS IT WOULD TREATING IT AS A CHARACTER
-    std::cout<<"Fully Compressed Text Size: " << compressedText.length() << " bytes\n";
-    std::cout<<"Total Compressed Size: "<<dictionarySize+compressedText.length()<<" bytes\n";
+    std::tuple<std::string, int, int> compressedText = compressAll(original, dict);
+    //Calculate compressed text size, 2 bytes per number referencing dictionary, 1 byte per punctuation/space
+    int compressedSize = (std::get<1>(compressedText) * 2) + std::get<2>(compressedText);
+    std::cout<<"Fully Compressed Text Size: " << compressedSize << " bytes\n";
+    std::cout<<"Total Compressed Size: "<<dictionarySize+compressedSize<<" bytes\n";
 
-    //std::cout<<compressedText<<'\n';
-
-    //Step 5
+    //Step 5 - find break even point
 
 }
